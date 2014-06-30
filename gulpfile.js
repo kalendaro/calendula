f0e67276a1a50b68ba0ef12d41c39f01b5c82404
Denis Seleznev <hcodes@yandex.ru>
2014-07-01 00:07:55 +0400

    Сжатие js и css

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var path = require('path');
 
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
+var cleancss = require('gulp-cleancss');
+var uglify = require('gulp-uglify');
 
 var paths = {
     js: [
@@ -24,17 +26,46 @@ var paths = {
 
 gulp.task('js', function() {
     return gulp.src(paths.js)
+        .pipe(concat('calendula.dev.js'))
+        .pipe(gulp.dest('./source'));
+});
+
+gulp.task('uglify', function() {
+    return gulp.src(paths.js)
         .pipe(concat('calendula.js'))
+        .pipe(uglify())
         .pipe(gulp.dest('./'));
 });
 
 gulp.task('css', function() {
     return gulp.src(paths.css)
+        .pipe(concat('calendula.dev.css'))
+        .pipe(gulp.dest('./source'));
+});
+
+gulp.task('cleancss', function() {
+    return gulp.src(paths.css)
         .pipe(concat('calendula.css'))
+        .pipe(cleancss({keepBreaks: false}))
         .pipe(gulp.dest('./'));
 });
 
 gulp.task('default', [
     'js',
-    'css'
+    'uglify',
+    'css',
+    'cleancss'
 ]);
+
+gulp.task('watch', function() {
+    gulp.run('js');
+    gulp.run('css');
+    
+    gulp.watch('source/js/**/*', function() {
+        gulp.run('js');
+    });
+    
+    gulp.watch('source/css/**/*', function() {
+        gulp.run('css');
+    });
+});
