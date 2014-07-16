cc28a39c10fc538298e118784c4281a16b2f12dd
Denis Seleznev <hcodes@yandex.ru>
2014-07-16 18:26:34 +0400

    Удаление ненужных файлов

--- gulpfile.js
+++ gulpfile.js
@@ -29,29 +29,20 @@ var paths = {
 };
 
 paths.devJs = paths.mainJs.concat('sources/js/locale/*.js', 'sources/js/end.js');
-paths.prodJs = paths.mainJs.concat('sources/js/locale/calendula.locale.en.js', 'sources/js/locale/calendula.locale.ru.js', 'sources/js/end.js');
 paths.prodJsBase = paths.mainJs.concat('sources/js/end.js');
 paths.prodJsAll = paths.mainJs.concat('sources/js/locale/calendula.locale.*.js', 'sources/js/end.js');
 
 paths.devCss = paths.mainCss.concat('sources/css/calendula.theme.*.css');
-paths.prodCss = paths.mainCss.concat('sources/css/calendula.theme.default.css');
 paths.prodCssAll = paths.mainCss.concat('sources/css/calendula.theme.*.css');
 
-var jsTasks = ['devJs', 'prodJs', 'prodJsBase', 'prodJsAll', 'prodJsLocales'],
-    cssTasks = ['devCss', 'prodCss', 'prodCssBase', 'prodCssAll', 'prodCssThemes'],
+var jsTasks = ['devJs', 'prodJsBase', 'prodJsAll', 'prodJsLocales'],
+    cssTasks = ['devCss', 'prodCssBase', 'prodCssAll', 'prodCssThemes'],
     allTasks = [].concat(cssTasks, jsTasks);
     
 gulp.task('devJs', function() {
     return gulp.src(paths.devJs)
         .pipe(concat('calendula.dev.js'))
         .pipe(gulp.dest('./dev'));
-});
-
-gulp.task('prodJs', function() {
-    return gulp.src(paths.prodJs)
-        .pipe(concat('calendula.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('./'));
 });
 
 gulp.task('prodJsBase', function() {
@@ -78,13 +69,6 @@ gulp.task('devCss', function() {
     return gulp.src(paths.devCss)
         .pipe(concat('calendula.dev.css'))
         .pipe(gulp.dest('./dev'));
-});
-
-gulp.task('prodCss', function() {
-    return gulp.src(paths.prodCss)
-        .pipe(concat('calendula.css'))
-        .pipe(cleancss({keepBreaks: false}))
-        .pipe(gulp.dest('./'));
 });
 
 gulp.task('prodCssBase', function() {
