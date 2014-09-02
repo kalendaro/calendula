1a07038ce2392f11886ccde8f213d588458a9292
seles <seles@yandex-team.ru>
2014-09-02 18:50:23 +0400

    Автоматически позиционировать календарь в видимой части страницы #15

--- sources/js/offset.js
+++ sources/js/offset.js
@@ -1,13 +1,46 @@
 extend(Cln.prototype, {
     _left: function(elem, x) {
-        elem.style.left = x + 'px';
+        elem.style.left = typeof x === 'number' ? x + 'px' : x;
     },
     _top: function(elem, y) {
-        elem.style.top = y + 'px';
+        elem.style.top = typeof y === 'number' ? y + 'px' : y;
     },
     _position: function(elem, coords) {
         this._left(elem, coords.left);
         this._top(elem, coords.top);
+    },
+    position: function(pos) {
+        var p = {left: 0, top: 0},
+            buttonWidth, buttonHeight,
+            buttonLeft, buttonTop;
+
+        if(typeof pos === 'string') {
+            b = pos.split('-');
+
+            p.left = buttonLeft;
+            switch(b[0]) {
+                case 'right':
+                    p.left += buttonWidth;
+                break;
+                case 'center':
+                    p.left += buttonWidth / 2;
+                break;
+            }
+
+            p.top = buttonTop;
+            switch(b[0]) {
+                case 'bottom':
+                    p.top += buttonHeight;
+                break;
+                case 'center':
+                    p.top += buttonHeight / 2;
+                break;
+            }
+        } else {
+            p = pos;
+        }
+
+        this._position(this._elem, pos);
     },
     _offset: function(elem) {
         var box = {top: 0, left: 0};
