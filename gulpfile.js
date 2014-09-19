513a50188b80d64496053fb41b696a5c40e24ea2
Denis Seleznev <hcodes@yandex.ru>
2014-09-19 23:38:12 +0400

    Подключить autoprefixer #20

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,13 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     rename = require('gulp-rename'),
     cleancss = require('gulp-cleancss'),
-    uglify = require('gulp-uglify');
-
+    autoprefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    apBrowsers = {
+        browsers: ['ie >= 9', 'Firefox >= 24', 'Chrome >= 26', 'iOS >= 5', 'Safari >= 6', 'Android > 2.3'];
+    },
+    destDir = './build';
+    
 var paths = {
     mainJs: [
         'sources/js/start.js',
@@ -53,63 +58,67 @@ paths.prodCssAll = paths.mainCss.concat('sources/css/calendula.theme.*.css');
 var jsTasks = ['devJs', 'prodJsBase', 'prodJsAll', 'prodJsLocales', 'prodJsHolidays'],
     cssTasks = ['devCss', 'prodCssBase', 'prodCssAll', 'prodCssThemes'],
     allTasks = [].concat(cssTasks, jsTasks, 'watch');
-
+    
 gulp.task('devJs', function() {
     return gulp.src(paths.devJs)
         .pipe(concat('calendula.dev.js'))
-        .pipe(gulp.dest('./build'));
+        .pipe(gulp.dest(destDir));
 });
 
 gulp.task('prodJsBase', function() {
     return gulp.src(paths.prodJsBase)
         .pipe(concat('calendula.base.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('./build'));
+        .pipe(gulp.dest(destDir));
 });
 
 gulp.task('prodJsAll', function() {
     return gulp.src(paths.prodJsAll)
         .pipe(concat('calendula.all.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('./build'));
+        .pipe(gulp.dest(destDir));
 });
 
 gulp.task('prodJsLocales', function() {
     return gulp.src(paths.prodJsLocales)
         .pipe(uglify())
-        .pipe(gulp.dest('./build'));
+        .pipe(gulp.dest(destDir));
 });
 
 gulp.task('prodJsHolidays', function() {
     return gulp.src(paths.prodJsHolidays)
         .pipe(uglify())
-        .pipe(gulp.dest('./build'));
+        .pipe(gulp.dest(destDir));
 });
 
 gulp.task('devCss', function() {
     return gulp.src(paths.devCss)
         .pipe(concat('calendula.dev.css'))
-        .pipe(gulp.dest('./build'));
+        .pipe(autoprefixer(apBrowsers))
+        .pipe(gulp.dest(destDir));
 });
 
 gulp.task('prodCssBase', function() {
     return gulp.src(paths.mainCss)
         .pipe(concat('calendula.base.css'))
+        .pipe(autoprefixer(apBrowsers))
         .pipe(cleancss({keepBreaks: false}))
-        .pipe(gulp.dest('./build'));
+        .pipe(gulp.dest(destDir));
 });
 
 gulp.task('prodCssAll', function() {
     return gulp.src(paths.prodCssAll)
         .pipe(concat('calendula.all.css'))
+        .pipe(autoprefixer(apBrowsers))
         .pipe(cleancss({keepBreaks: false}))
-        .pipe(gulp.dest('./build'));
+        .pipe(gulp.dest(destDir));
 });
 
 gulp.task('prodCssThemes', function() {
     return gulp.src(paths.prodCssThemes)
+        .pipe(autoprefixer(apBrowsers))
         .pipe(cleancss({keepBreaks: false}))
-        .pipe(gulp.dest('./build'));
+        .pipe(gulp.dest(destDir));
 });
 
 gulp.task('watch', function() {
